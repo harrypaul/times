f862d7a10c0a7c665e2ae8253a87320cc184b588
harrypaul <harrypaul@qq.com>
2014-05-20 16:39:36 +0800

    Update times.cpp

--- times.cpp
+++ times.cpp
@@ -1,1 +1,9 @@
 /*what is this ? just for fun, do u have time?*/
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  cout << " Yes!, i have time for fun, just for fun, life is too short!" << endl;
+  return 0;  
+}
